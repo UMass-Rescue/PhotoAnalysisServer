29ae3c24bca7a6a8e41da8fcbb40fe61ddf0111f
Senarii <vortekdynamic@gmail.com>
2020-09-30 20:23:09 -0400

    POST Predict saves file to disk and redis

--- server/main.py
+++ server/main.py
@@ -1,19 +1,20 @@
 from typing import List
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 
-
+import os
 import hashlib
-import redis
-import fastapi_plugins
+import redis as rd
 from pymongo import MongoClient
+import shutil
 from pydantic import BaseModel
 
 client = MongoClient('database', 27017)
 database = client['result_database']
 image_results = database['image_results']
+redis = rd.Redis(host='redis', port=6379)
 app = FastAPI()
 
 
@@ -43,43 +44,53 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    # connect to redis
-    client = redis.Redis(host='redis', port=6379)
-
-    # set a key
-    client.set('test-key', 'test-value')
-
-    # get a value
-    value = client.get('test-key')
-    return {"message": value}
+    return {"message": "PhotoAnalysisServer Running!"}
 
 
 @app.post("/predict")
 async def get_prediction(images: List[UploadFile] = File(...)):
-    BUF_SIZE = 65536 # Read image data in 64KB Chunks for hashlib
+    BUFFER_SIZE = 65536   # Read image data in 64KB Chunks for hashlib
     hashes = {}
 
+    # Process uploaded images
     for upload_file in images:
         f = upload_file.file
         md5 = hashlib.md5()
         while True:
-            data = f.read(BUF_SIZE)
+            data = f.read(BUFFER_SIZE)
             if not data:
                 break
             md5.update(data)
-        hashes[upload_file.filename] = md5.hexdigest()
+
+        # Process image
+        image_hash = md5.hexdigest()
+        hashes[upload_file.filename] = image_hash
+
+
+        # Save files to directory ./images/ and the images will automatically be saved locally via Docker
+        # The ./images/ folder on the host machine maps to ./server/images/ on Docker
+        file_extension = os.path.splitext(upload_file.filename)[1]
+        with open("./images/"+image_hash+file_extension, "wb") as buffer:
+            shutil.copyfileobj(f, buffer)
+
+        # Store current status of processing job in redis (false=not processed yet)
+        redis.set(image_hash, 'false')
+
+
+
 
     return {"images": [hashes[key] for key in hashes]}
 
 
-@app.get('/images')
-async def list_images():
-    images = []
-    for image in image_results.find():
-        images.append(ImageResult(**image))
-    return {'images': images}
+@app.get("/predict/{key}")
+async def get_job(key):
 
-@app.post('/images')
-async def process_image(image: ImageResult):
-    ret = image_results.insert_one(image.dict(by_alias=True))
-    return {'images': ret}
+    # Check that image exists in system
+    if not redis.exists(key):
+        return HTTPException(status_code=404, detail="key not found")
+
+    result = redis.get(key)
+
+    return {"status": result}
+
+
